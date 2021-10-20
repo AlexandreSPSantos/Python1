b45c670f64fac86e86dcf2b0b4e17f63f27fa3d8
AlexandreSPSantos <92729946+AlexandreSPSantos@users.noreply.github.com>
2021-10-20 12:10:34 -0300

    Update calculadora_v1.py

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -5,5 +5,30 @@
 # Assista o vídeo com a execução do programa!
 
 print("\n******************* Python Calculator *******************")
+oper = int(input("Informe 1 - Soma 2 - Subtracao 3 - multiplicacao 3 - divisao 5 potencia 6 - modulo"))
+n1 = float(input("Informe o primeiro número "))
+n2 = float(input("Informe o segundo número "))
+def calc(n1,n2):
+  sinal = 0 
+  if (oper==1):
+    sinal = '+'
+   elsif (oper==2):
+    sinal = '-'
+  if (oper==3):
+    sinal = '*'
+  if (oper==4):
+    sinal = '/'
+  if (oper==5):
+    sinal = '^'
+  if (oper==6):
+    sinal = '%'
+  calc = n1 + " " + sinal + " " + n2  
+ retrn(calc) 
+    
+     
+     
+   
+     
+        
 
 
